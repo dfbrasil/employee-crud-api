d94f2946c14264eaafa0f1736821e0a734c2f93c
Daniel Brasil <danieldfb@gmail.com>
2023-02-19 10:12:31 -0300

    CRUD Funcional

--- employee-api/src/routes/employee.routes.js
+++ employee-api/src/routes/employee.routes.js
@@ -13,4 +13,16 @@ const employeeController = require('../controllers/employee.controller');
 // ==> Rota responsável por criar um novo 'Colaborador(a)': (POST): localhost:3000/api/employees
 router.post('/employees', employeeController.createEmployee);
 
+// ==> Rota responsável por listar todos os 'Colaboradores': (GET): localhost:3000/api/employees
+router.get('/employees', employeeController.listAllEmployees);
+
+// ==> Rota responsável por listar um determinado 'Colaborador' por Id: (GET): localhost:3000/api/employees/:id
+router.get('/employees/:id', employeeController.findEmployeeById)
+
+// ==> Rota reponsável por atualizar um determinado 'Colaborador(a) por Id: (PUT): localhost:3000/api/employees/:id
+router.put('/employees/:id', employeeController.updateEmployeeById)
+
+// ==> Rota responsável por deletar/excluir um determinado 'Colaborador(a) por Id: localhost:3000/api/employees/:id
+router.delete('/employees/:id', employeeController.deleteEmployeeById);
+
 module.exports = router;
